d81fbaa5933b9eaa1fed7c36e8d9ca9495009c74
Jason <quizathaderat@yahoo.com>
2013-10-04 15:20:15 -0700

    Added permissions screen for admins

--- features/step_definitions/creating_projects_steps.rb
+++ features/step_definitions/creating_projects_steps.rb
@@ -34,13 +34,13 @@ When /^(?:|I )press "([^"]*)"$/ do |button|
   click_button(button)
 end
 
-# When /^(?:|I )follow "([^"]*)"$/ do |link|
-  # click_link(link)
-# end
-
-When(/^I follow "(.*?)"$/) do |link|
+When /^(?:|I )follow "([^"]*)"$/ do |link|
   click_link(link)
 end
+
+# When(/^I follow "(.*?)"$/) do |link|
+  # click_link(link)
+# end
 
 When /^(?:|I )fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
   fill_in(field, :with => value)
