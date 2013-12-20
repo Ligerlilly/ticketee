653f5324a561ee7bbe7696bca6ff67e4d7e1b950
Jason <quizathaderat@yahoo.com>
2013-12-20 13:36:19 -0800

    Admins can now set a default state for tickets

--- features/step_definitions/creating_comments_steps.rb
+++ features/step_definitions/creating_comments_steps.rb
@@ -1,10 +1,10 @@
-And(/^I follow "(.*?)" within "(.*?)"$/) do |link, element|
+And(/^I follow "([^"]*)" within "([^"]*)"$/) do |link, element|
   within(element) do
     click_link(link)
   end
 end
 
-Then(/^I should see "(.*?)" within "(.*?)"$/) do |text, element|
+Then(/^I should see "([^"]*)" within "([^"]*)"$/) do |text, element|
   within(element) do
     page.should have_content(text)
   end
