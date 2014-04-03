f0a2f10dacbf11f524071bbff8fe0eed4501c544
Jason <quizathaderat@yahoo.com>
2014-04-03 16:32:23 -0700

    Add Receiver class to receive emails

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,8 +1,12 @@
 class Notifier < ActionMailer::Base
   default from: "ticketee@gmail.com"
   def comment_updated(comment, user)
+    
     @comment = comment
     @user = user
-    mail(to: @user.email, subject: "[ticketee] #{comment.ticket.project.name} - #{comment.ticket.title}")  
+    mail(
+    from: "Ticketee <youraccount+#{comment.project.id}+#{comment.ticket.id}@example.com>",
+    to: @user.email, 
+    subject: "[ticketee] #{comment.ticket.project.name} - #{comment.ticket.title}")  
   end
 end
--- spec/mailers/notifier_spec.rb
+++ spec/mailers/notifier_spec.rb
@@ -1,34 +1,45 @@
-require "spec_helper"
-include Devise::TestHelpers
-describe Notifier do
-   let(:commenter) { FactoryGirl.create(:confirmed_user) }
-    let(:ticket_owner) { FactoryGirl.create(:confirmed_user) }
-    let(:project) { FactoryGirl.create(:project, name: "TextMate 2") }
- 
-    let(:ticket) { FactoryGirl.create(:ticket, project: project, user: ticket_owner, title: "Release date",
-      description: "TBA very shortly") }
-      
-    let(:comment) do
-       Comment.new({
-          ticket: ticket,
-          user: commenter,
-          text: "Is it Ready yet?"
-        }, without_protection: true)
+# require "spec_helper"
+# include Devise::TestHelpers
+# describe Notifier do
+   # let(:commenter) { FactoryGirl.create(:confirmed_user) }
+    # let(:ticket_owner) { FactoryGirl.create(:confirmed_user) }
+    # let(:project) { FactoryGirl.create(:project, name: "TextMate 2") }
+#  
+    # let(:ticket) { FactoryGirl.create(:ticket, project: project, user: ticket_owner, title: "Release date",
+      # description: "TBA very shortly") }
+#       
+    # let(:comment) do
+       # Comment.new({
+          # ticket: ticket,
+          # user: commenter,
+          # text: "Is it Ready yet?"
+        # }, without_protection: true)
+    # end
+#    
+    # let(:mail) { Notifier.comment_updated(comment, ticket_owner) }
+#     
+#     
+  # context "comment_update" do
+#    
+    # it "mail should be sent to ticket_owner" do
+        # mail.to.should include(ticket_owner.email)
+        # mail.subject.should include("[ticketee] TextMate 2 - Release date")     
+    # end
+#     
+  # end
+#   
+#   
+# 
+#    
+# end
+  require 'spec_helper'
+  include Devise::TestHelpers
+  
+  describe Notifier do
+    it "conrectly sets the reply-to" do
+      comment = FactoryGirl.create(:comment)
+      mail = ActionMailer::Base.deliveries.last
+      mail.from.should eql(["youraccount+#{comment.project.id}+" + "#{comment.ticket.id}@example.com"])
     end
-   
-    let(:mail) { Notifier.comment_updated(comment, ticket_owner) }
-    
-    
-  context "comment_update" do
-   
-    it "mail should be sent to ticket_owner" do
-        mail.to.should include(ticket_owner.email)
-        mail.subject.should include("[ticketee] TextMate 2 - Release date")     
-    end
-    
   end
-  
-  
 
-   
-end
