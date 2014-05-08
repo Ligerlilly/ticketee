e8efc986d4d9a3acc4ea195b126187b9c52db56c
Jason <quizathaderat@yahoo.com>
2014-05-08 16:58:27 -0700

    Restricting projects API show to only users who have permission to view a project

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,12 +1,10 @@
   class Api::V1::BaseController < ActionController::Base
     before_filter :authenticate_user
-    before_filter :authorize_admin!, except: [:index, :show]
+   
     respond_to :json, :xml
     
     private
-    def authenticate_user
-     @current_user = User.find_by_authentication_token(params[:token])
-    end
+    
     
     def current_user
       @current_user
@@ -18,12 +16,16 @@
         respond_with(error: "Token is invalid")
       end
     end
+    
+    def authorize_admin!   
+      if !@current_user.admin?
+        error = { error: "You must be an admin to do that." }
+        warden.custom_failure!
+        render params[:format].to_sym => error, status: 401
+      end
+    end
+    
   end
   
-  def authorize_admin!   
-    if !@current_user.admin?
-      error = { error: "You must be an admin to do that." }
-      warden.custom_failure!
-      render params[:format].to_sym => error, status: 401
-    end
-  end
+  
+  
